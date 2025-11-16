3a3cc72eb5a25298578ac2b0fa4a34c089a42518
ProofVault <evidenceonline2@gmail.com>
2025-11-17 00:11:50 +0100

    Delete proofvault-0.5/modules directory

--- proofvault-0.5/modules/shared_data/src/main/scala/io/proofvault/shared_data/model/TextUpdate.scala
+++
@@ -1,9 +0,0 @@
-package io.proofvault.shared_data.model
-
-import derevo.circe.magnolia.{decoder, encoder}
-import derevo.derive
-import org.tessellation.currency.dataApplication.DataUpdate
-
-/** TextUpdate using derevo annotations like NFT example */
-@derive(decoder, encoder)
-final case class TextUpdate(id: String, hash: String) extends DataUpdate
